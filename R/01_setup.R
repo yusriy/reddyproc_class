afb2d3cf6fbcf881dd80cc064d0f522bd3fcbb35
atmosfera5 <8579268+yusriy@users.noreply.github.com>
2023-07-13 18:55:25 +0800

    Added additional setup code copied from the original REddyProc repo.

--- R/01_setup.R
+++ R/01_setup.R
@@ -5,3 +5,20 @@
 # Load the Package
 # The library needs to be reloaded every time R or RStudio starts
 library(REddyProc)
+
+
+# install mlegp dependency which unfortunately is going to be archived from CRAN
+#install.packages("https://cran.r-project.org/src/contrib/mlegp_3.1.8.tar.gz", repos=NULL)
+# or 
+#install.packages("https://cran.r-project.org/src/contrib/Archive/mlegp/mlegp_3.1.7.tar.gz", repos=NULL)
+
+# Release stable version from CRAN
+#install.packages("REddyProc")
+
+# The development version from GitHub using devtools:
+# install.packages("devtools")
+#devtools::install_github("bgctw/REddyProc")
+
+#install.packages("tidyverse")
+#update.packages(oldPkgs="dplyr")
+
