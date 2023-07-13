ad653b97eb35d19b79b1cf139ab1e80ee41987d5
atmosfera5 <8579268+yusriy@users.noreply.github.com>
2023-07-13 18:44:46 +0800

    Added the setup code.

---
+++ R/01_setup.R
@@ -0,0 +1,7 @@
+#### Installing the Package #####
+# Install
+#install.packages('REddyProc')
+
+# Load the Package
+# The library needs to be reloaded every time R or RStudio starts
+library(REddyProc)
